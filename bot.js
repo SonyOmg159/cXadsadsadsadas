5761feae80a180424b7700d3aade86f5b6683893
SonyOmg159 <42983072+SonyOmg159@users.noreply.github.com>
2019-01-25 23:59:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'البرفكس حقك هنا'
+const prefix = 'P'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
