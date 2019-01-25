6b329cb9878f73eb6cfd34fb8871ec0fa45af656
SonyOmg159 <42983072+SonyOmg159@users.noreply.github.com>
2019-01-25 23:52:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,28 @@ client.on('message', msg => {
   }
 });
 
+
+ client.on('message',async message => {
+  if(message.content.startsWith(prefix + "setvoice")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('? **??? ???? ????????? ???????**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('? **??? ??? ????????? ???????**');
+  message.channel.send('?| **?? ??? ????? ?????**');
+  message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+    },1000);
+  });
+  }
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
+
+
